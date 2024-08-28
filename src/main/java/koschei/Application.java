ca7d9c1d64df9df6579247f4be5ed540bbd3ffd2
onlaynik <89087933259s@gmail.com>
2024-08-28 17:52:36 +0600

    first

---
+++ src/main/java/koschei/Application.java
@@ -0,0 +1,17 @@
+package koschei;
+
+import koschei.config.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        KoscheiTheDeathless koscheiTheDeathless =
+                context.getBean(KoscheiTheDeathless.class);
+
+        System.out.println(koscheiTheDeathless.getRulesByDeth());
+    }
+}
